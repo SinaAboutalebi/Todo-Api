8c08a185da10f6503f15a7aef13a7abbeb983b2e
Zer0Power <zeroopoower@gmail.com>
2022-08-19 18:22:56 +0430

    add dbConnection.js

---
+++ src/dbConnection.js
@@ -0,0 +1,21 @@
+//---------------------------💔🚬 'Zer0Power 💔🚬---------------------------//
+//Packages
+const mongoose = require("mongoose");
+const mongo_URI = 'ENTER YOUR Mongodb URI'; //EDIT HERE
+
+
+//Functions
+mongoose.connect(mongo_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then((db) => {
+    console.log("[✅] DB Connected Successfully.");
+}).catch((error) =>{
+    console.log("[❌] DB Failed To Connect.", error)
+})
+
+
+
+module.exports = exports = mongoose;
+
+//---------------------------💔🚬 'Zer0Power 💔🚬---------------------------//
